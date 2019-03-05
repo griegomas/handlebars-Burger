d6ba3d0109c2f4c63031799a2fae77f58deb7562
griegomas <38501062+griegomas@users.noreply.github.com>
2019-03-04 21:21:24 -0500

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 
 connection.connect(function(err) {
